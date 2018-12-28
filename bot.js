500e71ca8b8afe45e9d25825478546b0b16e5da9
Azoooooooooz <44898336+epic3azoozez135@users.noreply.github.com>
2018-12-28 03:33:00 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1630,55 +1630,6 @@ message.channel.send(`This avatar For ${user} link : ${user.avatarURL}`);
 
 
 
-var dat = JSON.parse("{}");
-function forEachObject(obj, func) {
-    Object.keys(obj).forEach(function (key) { func(key, obj[key]) });
-}
-client.on("ready", () => {
-    var guild;
-    while (!guild)
-        guild = client.guilds.get("526664748759253022");
-    guild.fetchInvites().then((data) => {
-        data.forEach((Invite, key, map) => {
-            var Inv = Invite.code;
-            dat[Inv] = Invite.uses;
-        });
-    });
-});
-
-
-
-client.on("guildMemberAdd", (member) => {
-    let channel = member.guild.channels.get("526669622951280651");
-    if (!channel) {
-        console.log("!the channel id it's not correct");
-        return;
-    }
-    if (member.id == client.user.id) {
-        return;
-    }
-    console.log('-');
-    var guild;
-    while (!guild)
-        guild = client.guilds.get("526664748759253022");
-    guild.fetchInvites().then((data) => {
-        data.forEach((Invite, key, map) => {
-            var Inv = Invite.code;
-            if (dat[Inv])
-                if (dat[Inv] < Invite.uses) {
- channel.send(`تم دعوته بواسطة  ${Invite.inviter} `) ;         
- }
-            dat[Inv] = Invite.uses;
-       
-       });
-    });
-});
-
-   
- 
-
-
-
 
    
 

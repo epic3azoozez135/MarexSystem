5ad17b8515d7968663860e463f42ebe79cc01092
Azoooooooooz <44898336+epic3azoozez135@users.noreply.github.com>
2018-12-29 18:02:16 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -2177,4 +2177,17 @@ client.on("message", (message) => {
 });
 
 
+
+
+client.on("guildMemberAdd", async member => {
+  let moment2 = require('moment-duration-format'),
+      moment = require("moment"),
+      date = moment.duration(new Date() - member.user.createdAt).format("d");
+
+  if(date < 25) {
+    member.ban("Member account age is lower than 25 days.")
+  }
+});
+
+
 client.login(process.env.BOT_TOKEN);
